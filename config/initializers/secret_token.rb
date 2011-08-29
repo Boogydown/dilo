96f907304fc38a82bd3122c8e4d649076065f636
Jason Craft <jason.craft@pearson.com>
2011-08-29 13:20:04 -0500

    adding dilo to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dilo::Application.config.secret_token = 'a113d7a5357357ed05da4b1f2d4e92891a80ff0c5d05088a1eb328ba5201229fe98713006e155acb5932d1ea56048d936a1387eddde5f68f7d49df3b3375bc0d'

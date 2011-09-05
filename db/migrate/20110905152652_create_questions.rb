58d1d6d2dd035cccf3c5da820e05490262b7a5a4
Jason Craft <jason.craft@pearson.com>
2011-09-05 10:52:46 -0500

    questions and responses

---
+++ db/migrate/20110905152652_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration
+  def self.up
+    create_table :questions do |t|
+      t.text :prompt
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :questions
+  end
+end

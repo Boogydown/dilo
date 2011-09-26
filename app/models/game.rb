808de7f8b1c2b9f5f0c5511204d16a47b013abe5
James Setaro <james.setaro@pearson.com>
2011-09-26 12:23:37 -0500

    trying to have autosave turned on.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,26 @@
 class Game < ActiveRecord::Base
-  has_many :questions, :through => :game_questions
+  #has_many :game_questions
+  #has_many :questions, :through => :game_questions
+  has_and_belongs_to_many :questions, :autosave => true
+
   has_many :sessions
+  #accepts_nested_attributes_for :game_questions, :questions
+
+  #def after_initialize
+  #  Bar.all.each do |bar|
+  #  wtfs << Wtf.new(:bar => bar, :data_i_need_to_set => 10)  # Rails should auto-assign :foo => self
+  #  bars << bar
+  #end
+
+
+  def as_json(options={})
+    super( :include =>[:questions])
+  end
+
+  def as_xml(options={})
+     super( :include =>[:questions])
+   end
+
+
+
 end

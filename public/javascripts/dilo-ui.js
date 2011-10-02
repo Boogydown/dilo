caa0c01b623c9245108e045cb0f9a0978cb29f2e
Jason Craft <jason.craft@pearson.com>
2011-10-02 13:43:30 -0500

    duh, it helps to add the document.ready wrapper

--- public/javascripts/dilo-ui.js
+++ public/javascripts/dilo-ui.js
@@ -1,93 +1,91 @@
-/*
- * shared scripts
- */
+$(document).ready(function() {
+    jQuery.fn.timer = function(selector, exitFunction) {
+        if(!$(this).children(selector + ":last-child").is(":visible")){
+            $(this).children(selector + ":visible")
+                .css("display", "none")
+                .next(selector).css("display", "block");
+        }
+        else if(exitFunction) {
+             exitFunction.apply();
+        }
+        else {
+            $(this).children(selector + ":visible")
+                .css("display", "none")
+            .end().children(selector + ":first")
+                .css("display", "block");
+        }
+    };
 
-jQuery.fn.timer = function(selector, exitFunction) {
-    if(!$(this).children(selector + ":last-child").is(":visible")){
-        $(this).children(selector + ":visible")
-            .css("display", "none")
-            .next(selector).css("display", "block");
-    }
-    else if(exitFunction) {
-         exitFunction.apply();
-    }
-    else {
-        $(this).children(selector + ":visible")
-            .css("display", "none")
-        .end().children(selector + ":first")
-            .css("display", "block");
-    }
-};
-
-/*
- * login scripts
- */
+    /*
+     * login scripts
+     */
 
 
-/*
- * waiting screen scripts
- */
+    /*
+     * waiting screen scripts
+     */
 
-$('#waiting').live('pagecreate', function(event){
-	$("#vocab-study .word-prompt:first").css("display", "block");
+    $('#waiting').live('pagecreate', function(event){
+        $("#vocab-study .word-prompt:first").css("display", "block");
 
-    var goToCountdown = function() {
-        location.href="#countdown";
-    }
+        var goToCountdown = function() {
+            location.href="#countdown";
+        }
 
-	window.setInterval(function() {
-		$("#vocab-study").timer(".word-prompt", goToCountdown);
-	}, 1300);
-});
-
-/*
- * countdown scripts
- */
-
-$('#countdown').live('pagecreate', function(event){
-	$("#downer h1:first").css("display", "block");
-
-    var goToQuestion = function() {
-       location.href='#question';
-    }
-
-	window.setInterval(function() {
-		$("#downer").timer("h1", goToQuestion);
-	}, 1300);
-});
-
-// question screen scripts
-$('#question').live('pagecreate', function(event){
-    $(".answerChoice").addClass("unguessed");
-
-    $(".answerChoice").bind('click', function(event) {
-         $(this).removeClass("unguessed");
-
-        if($(this).attr("correct") == "true")
-            $(this).addClass("correct-guess");
-         else
-            $(this).addClass("incorrect-guess");
-
-         $(this).siblings(".answerChoice").removeClass("unguessed");
-         $(this).siblings(".answerChoice").addClass("guessed");
-
-         $(".answerChoice").unbind(event);
-         clearInterval(scoreCountdown);
+        window.setInterval(function() {
+            $("#vocab-study").timer(".word-prompt", goToCountdown);
+        }, 1300);
     });
 
-    var scoreWidth = 100;
-    var score = Number($('div#scorebar').text());
-    var scoreIncrement = score * .05;
-    var scoreCountdown = window.setInterval(function(){
-         scoreWidth = scoreWidth - 5;
-         score = score - (scoreIncrement);
+    /*
+     * countdown scripts
+     */
 
-         if(score <= 0)
-            clearInterval(scoreCountdown);
+    $('#countdown').live('pagecreate', function(event){
+        $("#downer h1:first").css("display", "block");
 
-         $('div#scorebar').css('width', scoreWidth + '%');
-         $('div#scorebar').text(score);
-    }, 1000);
+        var goToQuestion = function() {
+           location.href='#question';
+        }
+
+        window.setInterval(function() {
+            $("#downer").timer("h1", goToQuestion);
+        }, 1300);
+    });
+
+    // question screen scripts
+    $('#question').live('pagecreate', function(event){
+        $(".answerChoice").addClass("unguessed");
+
+        $(".answerChoice").bind('click', function(event) {
+             $(this).removeClass("unguessed");
+
+            if($(this).attr("correct") == "true")
+                $(this).addClass("correct-guess");
+             else
+                $(this).addClass("incorrect-guess");
+
+             $(this).siblings(".answerChoice").removeClass("unguessed");
+             $(this).siblings(".answerChoice").addClass("guessed");
+
+             $(".answerChoice").unbind(event);
+             clearInterval(scoreCountdown);
+        });
+
+        var scoreWidth = 100;
+        var score = Number($('div#scorebar').text());
+        var scoreIncrement = score * .05;
+        var scoreCountdown = window.setInterval(function(){
+             scoreWidth = scoreWidth - 5;
+             score = score - (scoreIncrement);
+
+             if(score <= 0)
+                clearInterval(scoreCountdown);
+
+             $('div#scorebar').css('width', scoreWidth + '%');
+             $('div#scorebar').text(score);
+        }, 1000);
+    });
+
+    // game over scripts
 });
-
-// game over scripts

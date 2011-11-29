07ed5996922e014b2d49a7b74471063f27094300
Dimitri Hammond <dimitri@vyncup.com>
2011-11-29 01:28:20 -0600

    dropping puff for countdown - too slow on iPhone; going back to fontSize

--- public/javascripts/views/WaitTimerView.js
+++ public/javascripts/views/WaitTimerView.js
@@ -5,13 +5,28 @@ App.Views.WaitTimerView = App.Views.TimerView.extend({
 	renderPercent: function( p ) { /*do nothing*/ },
 	renderMS: function( ms ) {
 		var e = $(this.el);
-		e.css({display:"block"});
+		//e.css({display:"block"});
 		e.text( Math.ceil( ms / 1000 ) );
-		e.hide("puff",{
+		if (!this.startStyle)
+			this.startStyle = {
+				fontSize: e.css("fontSize"),
+				marginTop: e.css("marginTop"),
+				opacity: 1
+			};
+		e.css(this.startStyle).animate({
+			fontSize: "920px",
+			marginTop: "-347px",
+			opacity: 0
+		},{
+			duration:1100,
+			queue:false,
+			easing:"linear"
+		});
+/*		e.hide("puff",{
 			percent:800,
 			duration: 1100,
 			queue: false,
 			easing: "linear"
 		});
-	}
+*/	}
 });
